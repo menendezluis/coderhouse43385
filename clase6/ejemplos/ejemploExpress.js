906e41c0c85d47745dc617766c3a1d05fd0a72e9
Luis Menendez <lmenendez.1@gmail.com>
2023-06-29 18:01:05 -0600

    clase 6parte2

--- clase6/ejemplos/ejemploExpress.js
+++ clase6/ejemplos/ejemploExpress.js
@@ -2,16 +2,23 @@ import express from "express";
 
 const app = express();
 
+app.use(express.urlencoded);
+/*
 app.get("/", (req, res) => {
   res.send("Hola mundo desde express!");
 });
-
-app.get("/saludo", (req, res) => {
+*/
+app.get("/saludos", (req, res) => {
   res.send("Hola estoy cambiando para probar nodemon!");
 });
 
-app.listen(8080, () => {
-  console.log("Servidor escuchando en el puerto 8080 desde express");
+app.get("/saludo/:curso/:nombre/:apellido", (req, res) => {
+  const { curso, nombre, apellido } = req.params;
+  res.send(`Hola ${nombre} ${apellido}, bienvenido al curso de" ${curso}`);
+});
+
+app.listen(3001, () => {
+  console.log("Servidor escuchando en el puerto 3001 desde express");
 });
 // //paso1 npm init -y
 // //paso2 configurar package.json agregar "type": "module",
