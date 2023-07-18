e4c0eb439ba69c0ae7bbc0eaef83c06e06e7ced5
Luis Menendez <lmenendez.1@gmail.com>
2023-07-19 01:52:34 +0200

    clase 11 parte2

--- clase11/public/js/index.js
+++ clase11/public/js/index.js
@@ -1,7 +1,67 @@
 const socket = io();
 
+let user;
+
+let chatBox = document.getElementById("chatBox");
+
 Swal.fire({
-  title: "Bienvenido al chat",
-  text: "Alerta basica con sweet alert",
-  icon: "success",
+  title: "Inicia sesion!",
+  text: "Ingresa tu nombre de usuario",
+  input: "text",
+  confirmButtonText: "Cool",
+  allowOutsideClick: false,
+  inputValidator: (value) => {
+    if (!value) {
+      return "Debe ingresar un nombre de usuario";
+    }
+  },
+}).then((result) => {
+  if (result.value) {
+    user = result.value;
+    socket.emit("new-user", { user: user, id: socket.id });
+  }
+});
+
+chatBox.addEventListener("keyup", (e) => {
+  console.log("chatBox existe", chatBox);
+  console.log("evento key up", e.key, chatBox.value);
+  if (e.key === "Enter") {
+    if (chatBox.value.trim().length > 0) {
+      socket.emit("message", {
+        user: user,
+        message: chatBox.value,
+      });
+      chatBox.value = "";
+    }
+  }
+});
+
+socket.on("messageLogs", (data) => {
+  let log = document.getElementById("messageLogs");
+  let message = "";
+
+  data.forEach((elem) => {
+    message += `
+     
+        <div class="chat-message">
+        <div class="message-bubble">
+  
+          <div class="message-sender">${elem.user}</div>
+          <p>${elem.message}</p>
+          </div>
+  
+        </div>
+      `;
+  });
+
+  log.innerHTML = message;
+});
+
+socket.on("new-user-connected", (data) => {
+  if (data.id !== socket.id)
+    Swal.fire({
+      text: `${data.user} se ha conectado al chat`,
+      toast: true,
+      position: "top-end",
+    });
 });
--- clase11/src/app.js
+++ clase11/src/app.js
@@ -12,7 +12,8 @@ const httpServer = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-const socket = new Server(httpServer);
+let messages = [];
+const io = new Server(httpServer);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,3 +25,19 @@ app.set("views", "./src/views");
 app.use(express.static("public"));
 
 app.use("/", viewsRouter);
+
+io.on("connection", (socket) => {
+  console.log("Nuevo cliente conectado!");
+  socket.on("new-user", (data) => {
+    socket.user = data.user;
+    socket.id = data.id;
+    io.emit("new-user-connected", {
+      user: socket.user,
+      id: socket.id,
+    });
+  });
+  socket.on("message", (data) => {
+    messages.push(data);
+    io.emit("messageLogs", messages);
+  });
+});
